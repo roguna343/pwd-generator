9b2184d294657bc8da9db6aa8095d19fcbf25186
Rawlings Oguna <roguna7000@gmail.com>
2019-12-23 19:48:35 -0500

    Redefined import statements.

--- pwd-generator.py
+++ pwd-generator.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-import secrets
-import string
-import sys
+from secrets import choice # most secure module for generating random passwords
+from string import ascii_letters, digits # imports all letters A-Z lowercase and uppercase, and all 10 digits
+from sys import argv # allows use of command-line arguments
 
-chars = string.ascii_letters + string.digits + "!@#$%&*?"
+characters = ascii_letters + digits + "!@#$%&*?" # initializing our selection of characters
 
-number = int(sys.argv[1])
-passlen = int(sys.argv[2])
+number = int(argv[1]) # number of passwords to be printed
+pass_length = int(argv[2]) # length of each password
 
 def generate_pwd():
-    password = "".join(secrets.choice(chars) for i in range(passlen))
+    password = "".join(choice(characters) for i in range(pass_length))
     yield password
 
-print(f"Printing {number} password(s) of length {passlen}:")
+print(f"Printing {number} password(s) of length {pass_length}:")
 
+# displays all passwords
 for i in range(number):
     print(generate_pwd().__next__())
